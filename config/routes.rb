ed846dd0d6616025ec46199642d3378ca187f34b
wangjingru <w15673142360@gmail.com>
2017-07-06 17:11:04 +0800

    step1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :friendships
+  resources :users, only: [:index]
+
+  resources :conversations, only: [:index, :create] do
+    resources :messages
+  end
+
   root "friendships#index"
 end
